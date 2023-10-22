f2b75c18c219d4e4b83b49edbdfbf4259a947407
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2023-10-22 18:28:30 +1100

    Added media key support to the knob

--- keyboards/keychron/q1_pro/ansi_knob/keymaps/SaschaCowley/keymap.c
+++ keyboards/keychron/q1_pro/ansi_knob/keymaps/SaschaCowley/keymap.c
@@ -16,6 +16,31 @@
 
 #include QMK_KEYBOARD_H
 
+// Tap dance declarations
+enum {
+    TD_KNOB_BUTTON,
+};
+
+// Tap dance definitions
+void tap_dance_media(tap_dance_state_t *state, void *data) {
+    switch (state->count) {
+        case 1:
+            tap_code(KC_MEDIA_PLAY_PAUSE);
+            break;
+        case 2:
+            tap_code(KC_MEDIA_NEXT_TRACK);
+            break;
+        case 3:
+            tap_code(KC_MEDIA_PREV_TRACK);
+            break;
+    }
+    reset_tap_dance(state);
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_KNOB_BUTTON] = ACTION_TAP_DANCE_FN(tap_dance_media),
+};
+
 // clang-format off
 enum layers{
   MAC_BASE,
@@ -42,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
     [WIN_BASE] = LAYOUT_ansi_82(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_MUTE,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             TD(TD_KNOB_BUTTON),
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_DEL,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_HOME,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_END,
